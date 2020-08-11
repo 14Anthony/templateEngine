4e34c9453640bc2c072b8eccdd3b4ccab3c9fa91
Anthony Belcastro <belcastt@live.com>
2020-08-10 19:24:29 -0700

    copy pasta, engineering class inheriting the Employee class, and changing github to school, and Engineer to Inter

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require('./Employee');
+
+class Engineer {
+    constructor(name, id, email, github) {
+        super(name, id, email, github);
+        this.school = school;
+        this.role = 'Intern'
+    }
+    get gitHub() {
+        return this.school;
+    }
+}
+
+module.exports = Intern;
