4f7c86d15939049fc104ec7fcc205826c79738d3
Anthony Belcastro <belcastt@live.com>
2020-08-10 19:47:27 -0700

    manager, engineer, employee pass , intern test not running

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,14 +1,15 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
 const Employee = require('./Employee');
 
-class Manager {
-    constructor(name, id, email,) {
-        super(name, id, email, extension);
-        this.extension = extension;
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email, officeNumber);
         this.role = 'Manager'
+        this.officeNumber = officeNumber;
     }
-    get gitHub() {
-        return this.extension;
+
+    getOfficeNumber() {
+        return this.officeNumber;
     }
 }
 
